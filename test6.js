d05e7651957621a55fda1a960a7d2c8e7c0fc990
mahipal-k-crest <133738835+mahipal-k-crest@users.noreply.github.com>
2023-05-30 11:41:27 +0530

    Update and rename test5.js to test6.js

--- test6.js
+++ test6.js
@@ -15,10 +15,9 @@
   secondSpan.id = 'closeButtonForPopupMessage';
   secondSpan.className = 'pop-up-message-close-icon';
   var svgElement = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-  svgElement.setAttribute('style', 'height: 18px; width: 18px;');
   svgElement.setAttribute('viewBox', '0 0 24 24');
-  svgElement.setAttribute('height', 18)
-  svgElement.setAttribute('width', 18)
+  svgElement.setAttribute('height', 22)
+  svgElement.setAttribute('width', 22)
 
   var pathElement = document.createElementNS('http://www.w3.org/2000/svg', 'path');
   pathElement.setAttribute('d', 'M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z');
@@ -270,6 +269,8 @@
   chatbotDiv.appendChild(chatContainerBrandDiv);
   document.body.appendChild(chatbotDiv);
 
+
+
   // pop-up-message open-close button  handler 
 
   const popupMessageContainer = document.querySelector("#popupMessageContainer");
@@ -334,5 +335,4 @@
       botIntroMessage.style.display = "none";
     }
   });
-
 
